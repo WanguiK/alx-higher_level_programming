69019cfc0a18e20f7dab4192fe2e7dddf28c81a1
WanguiK <wanguikings@gmail.com>
2023-08-08 06:33:15 -0700

    To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        if ord('a') <= ord(char) <= ord('z'):
+            print(chr(ord(char) - 32), end='')
+        else:
+            print(char, end='')
