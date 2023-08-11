9063564ad98d648d72351d1d10e16d58045f02c1
WanguiK <wanguikings@gmail.com>
2023-08-11 07:28:34 -0700

    Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    def add_arguments(argvs):
+        total = 0
+        for arg in argvs:
+            total += int(arg)
+        return total
+    argvs = sys.argv[1:]
+    result = add_arguments(argvs)
+    print(result)
