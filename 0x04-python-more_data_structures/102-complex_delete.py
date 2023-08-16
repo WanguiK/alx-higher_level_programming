bf81f3d16df2cd3688aea1c9a7eeab55295230d5
WanguiK <wanguikings@gmail.com>
2023-08-16 06:03:30 -0700

    Delete by value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -5,5 +5,5 @@ def complex_delete(a_dictionary, value):
         if i == value:
             keys_to_delete.append(key)
     for key in keys_to_delete:
-            del a_dictionary[key]
+        del a_dictionary[key]
     return (a_dictionary)
