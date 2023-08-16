3e039ed44ecdc8e4e4fc280f841b0c7d6292a7f4
WanguiK <wanguikings@gmail.com>
2023-08-16 03:23:47 -0700

    Multiply by 2

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    new_dictionary = {}
+    for key, i in a_dictionary.items():
+        new_dictionary[key] = (i * 2)
+    return (new_dictionary)
