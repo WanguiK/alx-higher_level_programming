e805bb4cbb79e872a06dddafdf8a9d9977ed932e
WanguiK <wanguikings@gmail.com>
2023-08-28 16:03:07 +0000

    Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,10 @@
-afe_print_list(my_list=[], x=0):
+#!/usr/bin/python3
+
+def afe_print_list(my_list=[], x=0):
     try:
         count = 0
         for i in range(x):
-            print(my_list[i], end="")
+            print("{}". format(my_list[i]), end="")
             count += 1
     except IndexError:
         pass
