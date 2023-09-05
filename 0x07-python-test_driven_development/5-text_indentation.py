9205c613ec615ff0fbd64bd5ba45069b90e6cee6
WanguiK <wanguikings@gmail.com>
2023-09-05 07:32:50 -0700

    Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+    This module prints a text with 2 new lines after each of these
+    characters: ., ? and : using the following methods:
+    text_indentation
+"""
+
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these characters: ., ? and :
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    
+    for char in text:
+        print(char, end="")
+        if char in [".", "?", ":"]:
+            print("\n\n", end="")
